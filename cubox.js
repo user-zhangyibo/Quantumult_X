649f571603d1539bbde6405788060b9e740c0706
user-zhangyibo <63438943+user-zhangyibo@users.noreply.github.com>
2023-05-20 11:54:13 +0800

    Update cubox.js

--- cubox.js
+++ cubox.js
@@ -12,7 +12,7 @@
 
 [rewrite_local]
 
-^https:\/\/cubox\.pro\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/cubox.js
+^https:\/\/cubox\.pro\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/user-zhangyibo/Quantumult_X/main/cubox.js
 
 [mitm] 
 
